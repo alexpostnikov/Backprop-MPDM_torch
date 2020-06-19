fix angle rotation in HSFM

scripts/MPDM/HSFM.py
@@ -1,6 +1,6 @@
 import torch
 from MPDM.RepulsiveForces import RepulsiveForces
-
+import numpy as np
 
 class HSFM:
     def __init__(self, param, DT=0.4):
@@ -8,22 +8,23 @@ class HSFM:
         self.rep_f = RepulsiveForces(self.param)
         self.DT = self.param.DT  # self.DT = DT
         # positive constant parameters
-        self.kf = 1.  # forward
-        self.ko = 0.3  # ortogonal
+        self.kf = 0.8  # forward
+        self.ko = 0.1  # ortogonal
         self.kd = torch.zeros(2)  # velocity
-        self.kd[1] = 5.0  # check this koef
+        self.kd[1] = 1.0  # check this koef
         self.Kb = torch.zeros(2, 2)  # Body k matrix
         self.Kb[0, 0] = self.kf
         self.Kb[1, 1] = self.ko
-        self.Vo = torch.tensor(1.0)  # pedestrians_goal_speed
+        self.Vo = torch.tensor(5.0)  # pedestrians_goal_speed
+        self.Vor = torch.tensor(5.0) # robot_goal_speed
 
-        self.kfi = 0.3  # angular control
-        self.kfig = 0.1  # angular speed control
+        self.kfi = 0.8  # angular control
+        self.kfig = 0.05  # angular speed control
         self.alpha = 3.  # koef needed to calculate kfi and kfig
         self.kj = 0.02  # koef needed to calculate kfi and kfig
 
-        self.ped_mass = 80
-        self.ped_radius = 0.4
+        self.ped_mass = 70
+        self.ped_radius = 0.35
         # inercial moment of ped (m*r^2/2)
         self.I = torch.tensor(self.ped_mass*self.ped_radius**2.*0.5)
 
@@ -32,11 +33,12 @@ class HSFM:
 
         # self.Vo = self.param.pedestrians_speed # ? Vo - maybe "the pedestrian’s desire to move with a given velocity vector"
 
+
     def force2U(self, forces, state):
         ped_angles = state[:, 2].clone()
-        ped_angular_speeds = state[:, 5].clone()
-        ped_speeds = state[:, 1:3].clone()
-        force_angles = forces[:, 2].clone()
+        ped_angular_speeds = state[:, 5]
+        ped_speeds = state[:, 1:3]
+        force_angles = forces[:, 2]
         Rots = torch.zeros(len(state), 2, 2)  # rotation from global to Body
         cos_array = torch.cos(ped_angles)
         sin_array = torch.sin(ped_angles)
@@ -46,7 +48,7 @@ class HSFM:
         Rots[:, 1, 1] = cos_array[:]
         # control input to HLM in Body frame
         # TODO:
-        Ub = (self.Kb@Rots@forces[:, :2].T)[0].T.reshape(3, 2) - self.kd * self.Vo
+        Ub = (self.Kb@(Rots.T.permute(2,0,1)@forces[:, :2].T)[0]).T #- self.kd * self.Vo
         Ufi = -self.kfi*(ped_angles-force_angles) - self.kfig * ped_angular_speeds  # angular control input to HLM
         return Ub, Ufi, Rots
 
@@ -55,23 +57,21 @@ class HSFM:
 
         Vb = (1/self.ped_mass)*Ub  # calc linear velocities in Body frame
         # rotate that velocities into global frame
-        state[:, 3:5] = (Rots.T.permute(2, 0, 1)@Vb.T)[0].T
-        state[:, :2] = state[:, :2].clone() + state[:, 3:5].clone() * \
-            self.DT  # move
+        state[:, 3:5] = (Rots@Vb.T)[0].T
 
         b = torch.zeros(len(state))  # inercial moment
         b[:] = 1/self.I  # inercial moment
         state[:, 5] = b.matmul(Ufi)  # calc angular speed
-        state[:, 2] = state[:, 2].clone() + state[:, 5].clone() * \
-            self.DT  # update self angle
+        
+        state[:, :3] = state[:, :3] + state[:, 3:] * self.DT  # move
         return state
 
     def calc_cost_function(self, robot_goal, robot_init_pose, agents_state):
         a = self.param.a
         b = self.param.b
         e = self.param.e
-        robot_pose = agents_state[0, :3].clone()
-        robot_speed = agents_state[0, 3:].clone()
+        robot_pose = agents_state[0, :3]
+        robot_speed = agents_state[0, 3:]
         if torch.norm(robot_init_pose - robot_goal) < 1e-6:
             # torch.ones(robot_pose.shape).requires_grad_(True)
             PG = torch.tensor([0.01])
@@ -92,40 +92,44 @@ class HSFM:
         return B
 
     def calc_forces(self, state, goals):
-        rep_force = self.rep_f.calc_rep_forces(
-            state[:, 0:2], state[:, 3:5], param_lambda=1)
+        rep_force = self.rep_f.calc_rep_forces(state[:, 0:2], state[:, 3:5], param_lambda=1)
         attr_force = self.force_goal(state, goals)
-        return rep_force, attr_force
+        F = rep_force + attr_force
+        #  calc force direction
+        F[:,2] = self.calc_phi(F[:,:2].clone())
+        return F
+
+    def calc_phi(self, F):
+        out = []
+        for v in F:
+            if v[0] > 0:
+                out.append(torch.atan(v[1] / v[0]))
+            else:
+                if v[1] > 0:
+                    if v[0] < 0:
+                        out.append(np.pi + torch.atan(v[1] / v[0]))
+                    else:
+                        out.append(np.pi)
+                elif v[1] < 0:
+                    if v[0] < 0:
+                        out.append(-np.pi + torch.atan(v[1] / v[0]))
+                    else:
+                        out.append(-np.pi)
+                else:
+                    out.append(0.0)
+        return torch.stack(out)
 
     def force_goal(self, input_state, goal):
-        num_ped = len(input_state)
-        k = self.param.socForcePersonPerson["k"] * torch.ones(num_ped)
+        k = self.param.socForcePersonPerson["k"] * torch.ones(len(input_state))
         k[0] = self.param.socForceRobotPerson["k"]
         k = k.view(-1, 1)
 
-        ps = self.param.pedestrians_speed
-        rs = self.param.robot_speed
         desired_direction = goal[:, 0:3] - input_state[:, 0:3] + 1e-6
         v_desired_x_y_yaw = torch.zeros_like(desired_direction)
-        norm_direction_lin = torch.sqrt(desired_direction.clone()[:, 0:1] ** 2 +
-                                        desired_direction.clone()[:, 1:2] ** 2)
-        norm_direction_rot = desired_direction.clone()[:, 2]
-
-        # v_desired_ = torch.sqrt(v_desired_x_y_yaw.clone()[:, 0]**2+v_desired_x_y_yaw.clone()[:, 1]**2+v_desired_x_y_yaw.clone()[:, 2]**2)
-        # torch.sqrt(
-        #     v_desired_x_y_yaw.clone()[:, 0]**2 +
-        #     v_desired_x_y_yaw.clone()[:, 1]**2 +
-        #     v_desired_x_y_yaw.clone()[:, 2]**2)
-        v_desired_x_y_yaw[1:, 0:2] = desired_direction[1:,
-                                                       0:2] * ps / norm_direction_lin[1:, 0:2]
-        v_desired_x_y_yaw[0, 0:2] = desired_direction[0,
-                                                      0:2] * ps / norm_direction_lin[0, 0:2]
-
-        # TODO: create param: desired rot speed
-        v_desired_x_y_yaw[1:, 2] *= desired_direction[1:,
-                                                      2] / norm_direction_rot[1:]
-        v_desired_x_y_yaw[0, 2] *= desired_direction[0, 2] / \
-            norm_direction_rot[0]
+        norm_direction_lin = torch.sqrt(desired_direction[:, 0:1] ** 2 +
+                                        desired_direction[:, 1:2] ** 2)
+        v_desired_x_y_yaw[:, 0:2] = desired_direction[:,0:2] * self.Vo / norm_direction_lin[:, 0:2]
+        
         # print (pedestrians_speed)
-        F_attr = k * (v_desired_x_y_yaw - input_state[:, 3:])
+        F_attr = k * (v_desired_x_y_yaw - input_state[:, 3:]*self.DT)
         return F_attr

scripts/MPDM/Layer.py
@@ -21,8 +21,7 @@ class Linear(nn.Module):
         robot_init_pose = stacked_state[0][0][:3]
         # state = 1 * input_state
         
-        rf, af = self.tm.calc_forces(state, goals)
-        F = rf + af
+        F = self.tm.calc_forces(state, goals)
         out = self.tm.pose_propagation(F, state.clone())
 
         current_cost = self.tm.calc_cost_function(